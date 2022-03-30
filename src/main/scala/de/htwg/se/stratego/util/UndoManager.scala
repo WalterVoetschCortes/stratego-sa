6ff57502e03ec889e7e64b1b20127fcece856074
WalterVoetschCortes <walter.vxd@googlemail.com>
2022-03-30 11:10:54 +0200

    rework(UndoManager): use currying

--- src/main/scala/de/htwg/se/stratego/util/UndoManager.scala
+++ src/main/scala/de/htwg/se/stratego/util/UndoManager.scala
@@ -1,5 +1,9 @@
 package de.htwg.se.stratego.util
 
+enum Step (val step: String):
+  case UNDO extends Step("UNDO")
+  case REDO extends Step("REDO")
+
 class UndoManager :
   private var undoStack: List[Command]= Nil
   private var redoStack: List[Command]= Nil
@@ -7,19 +11,23 @@ class UndoManager :
     undoStack = command::undoStack
     command.doStep
 
-  def undoStep  = 
-    undoStack match 
-      case  Nil =>
-      case head::stack => 
-        head.undoStep
-        undoStack=stack
-        redoStack= head::redoStack
+  def step(step: Step) = 
+    if(step == Step.UNDO)
+      undoStack match 
+        case  Nil =>
+        case head::stack => 
+          head.undoStep
+          undoStack=stack
+          redoStack= head::redoStack
+    else
+      redoStack match 
+        case Nil =>
+        case head::stack => 
+          head.redoStep
+          redoStack=stack
+          undoStack=head::undoStack
+
+  def undoStep  = step(Step.UNDO)
     
-  def redoStep = 
-    redoStack match 
-      case Nil =>
-      case head::stack => 
-        head.redoStep
-        redoStack=stack
-        undoStack=head::undoStack
+  def redoStep = step(Step.REDO)
     
